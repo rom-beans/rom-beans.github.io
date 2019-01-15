9757f79f07f4fc65a256a33d145a8d9b384b1d78
eulb-beans <46665055+eulb-beans@users.noreply.github.com>
2019-01-15 21:46:00 +0800

    Reset Timer

---
+++ rom/sea/timer/script.js
@@ -0,0 +1,20 @@
+updateTimer();
+
+function updateTimer() {
+    var time = new Date();
+        days = 6 - time.getUTCDay() + 1,
+        hours = 23 - time.getUTCHours() - 2,
+        minutes = 59 - time.getUTCMinutes(),
+        seconds = 59 - time.getUTCSeconds();
+
+
+document.getElementById('countdown-timer-days-w').innerHTML  = ("0" + days).slice(-2);
+document.getElementById('countdown-timer-hours-w').innerHTML  = ("0" + hours).slice(-2);
+document.getElementById('countdown-timer-minutes-w').innerHTML  = ("0" + minutes).slice(-2);
+document.getElementById('countdown-timer-seconds-w').innerHTML  = ("0" + seconds).slice(-2);
+
+document.getElementById('countdown-timer-hours').innerHTML  = ("0" + hours).slice(-2);
+document.getElementById('countdown-timer-minutes').innerHTML  = ("0" + minutes).slice(-2);
+document.getElementById('countdown-timer-seconds').innerHTML  = ("0" + seconds).slice(-2);
+
+}; setInterval('updateTimer()', 1000 ) // auto update time every 1 sec
